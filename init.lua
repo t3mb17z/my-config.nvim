bc9a040b80d02d10e4f70f44187d754b4a40bbc5
C1ENC14SOD4 <johnthesk23@gmail.com>
2025-02-10 18:43:29 -0400

    Last device commit

--- init.lua
+++ init.lua
@@ -15,3 +15,16 @@ vim.o.updatetime = 200
 
 require("config.lazy")
 require("config")
+local bufnr = vim.api.nvim_create_buf(true, false)
+local win = vim.api.nvim_open_win(bufnr, true, {
+	relative = 'editor',
+	width = 45,
+	height = 23,
+	row = 1, col = 1,
+})
+vim.bo[bufnr].modifiable = true
+local tem = vim.api.nvim_open_term(bufnr, {})
+if tem == 0 then
+  print("Error")
+end
+vim.api.nvim_chan_send(tem, "ls\n")
